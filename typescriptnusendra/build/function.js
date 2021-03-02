741a2a75876e3dec4aeaa43168ab22d944464171
Gegeact <gegeact.dev@gmail.com>
2021-03-02 09:21:47 +0700

    video 8

---
+++ typescriptnusendra/build/function.js
@@ -0,0 +1,13 @@
+"use strict";
+// typedata pada balika function
+function getName() {
+    return "Hello, My name is Gege";
+}
+console.log(getName());
+function getAge() {
+    return 123;
+}
+function printName() {
+    console.log("print name");
+}
+printName();
---
+++ typescriptnusendra/src/function.ts
@@ -0,0 +1,19 @@
+// typedata pada balika function
+
+function getName(): string {
+    return "Hello, My name is Gege";
+}
+
+console.log(getName());
+
+function getAge(): number {
+    return 123;
+
+}
+
+function printName(): void {
+    console.log("print name");
+}
+
+printName();
+
